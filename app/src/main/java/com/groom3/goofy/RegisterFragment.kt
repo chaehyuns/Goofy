705ef79bb0688b9092419864faea1b011dece2f7
이영준 <leeyoungjun@iyeongjun-ui-MacBookPro.local>
2022-11-17 01:19:16 +0900

    Chore

---
+++ app/src/main/java/com/groom3/goofy/RegisterFragment.kt
@@ -0,0 +1,37 @@
+package com.groom3.goofy
+
+import android.os.Bundle
+import android.util.Log
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.findNavController
+import com.groom3.goofy.databinding.FragmentRegisterBinding
+
+class RegisterFragment : Fragment() {
+    private lateinit var binding : FragmentRegisterBinding
+    private lateinit var kakaoAuthViewModel: KakaoAuthViewModel
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        binding = DataBindingUtil.inflate(inflater,R.layout.fragment_register,container,false)
+        kakaoAuthViewModel = ViewModelProvider(this).get(KakaoAuthViewModel::class.java)
+        binding.kakaoRegisterViewModel = kakaoAuthViewModel
+        binding.kakaoRegisterButton.setOnClickListener{
+            kakaoAuthViewModel.handleKakaoLogin()
+        }
+
+        kakaoAuthViewModel.accessToken.observe(viewLifecycleOwner) {
+            Log.d("MYTAG", "token is ${it}")
+        }
+        return binding.root
+
+    }
+}
